4cb1676b6c73c5a8a881faa6610f5fb7fa49188b
Pawel Flajszer <45766938+pflajszer@users.noreply.github.com>
2019-04-01 19:00:18 +0100

    Update application.py

--- application.py
+++ application.py
@@ -5,41 +5,6 @@ Created on Saturday, 23 March 2019 at 16:19
 
 @author: Pawel Flajszer
 
-Bugs and flaws:
-- FPS optimisation. so far doesn't go further than ~25fps. print(FPSCLOCK) to check.
-- pause the game and wait till fraction of the second before next level up, unpause, 
-       the level goes up, pause again, etc
-- Sound on/off toggle
-- change lists iteration to group iteration when drawing sprites etc.
-
-Bad design practices:
-- from <moduleName> import * (can cause trouble)
-- cluttered functions that perhaps could be simpler, and some of them could be class methods
-
-TODO:
-- implement more modes and make it a list. then access by "if item not in modes execute ..code".
-- ability to shoot enemies
-- smoke when accelerating
-
-
-Game controls:
-_______________________________________________________________________
-
-                            General:
-
-M                               - mute the music
-P                               - pause the game
-TAB                             - toggle tests
-
-Player 1:                       Player 2:
-arrow UP                        w               - move up
-arrow DOWN                      s               - move down
-arrow LEFT                      a               - move left
-arrow RIGHT                     d               - move right  
-
-_________________________________________________________________________
-"""
-
 # Import modules
 import pygame, random, sys
 from pygame.locals import *
